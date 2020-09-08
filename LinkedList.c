bd1e3cc28043637b7ad8f977a538b3da2b0916b7
Danny D <dannydbob@hotmail.com>
2020-09-08 16:05:57 +0100

    Some Changes

--- LinkedList.c
+++ LinkedList.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 struct List{
-	char name[100];//stores node value
+	char name[100];
 	struct List *next;//stores next nodes memory address
 };
 
@@ -17,11 +17,11 @@ int main() {
 	//Set struct member head to null
 	struct List *head = NULL;
 	char string[100];
-	int option = 0x1;
+	int option = 0x1;//Set option equal to 1
 	
-	while (option != 0x0) {
+	while (option != 0x0) {//While 0 isn't entered
 		menu();
-		scanf_s("%x", &option);
+		scanf_s("%x", &option);//Take in hex value for number
 		
 		switch (option) {
 			case 0x1:
@@ -63,24 +63,23 @@ void menu() {
 	printf("0 To quit menu\n");
 	printf("1 To add a node at the begining of the list\n");
 	printf("2 To add a node at the end of the list\n");
-	printf("3 To delete a node in the list by a value\n");
-	printf("4 Display list values\n");
+	printf("3 To delete a node in the list by a name\n");
+	printf("4 Display names in list\n");
 }
 
-void addNodeAtBegining(struct List** headPtr,char string[]) {//** is pointer to a pointer
+void addNodeAtBegining(struct List** headPtr,char string[]) {//** Is memory address to a pointer
 
-	//allocate dynamic memory for a new node in a linked list
+	//Allocate dynamic memory for a new node in a linked list
 	struct List *newNode = (struct List*)malloc(sizeof(struct List));
 	
-	//check if memory has been allocated
+	//Check if memory has been allocated
 	if (newNode == NULL) {
 		printf("Memory not allocated\n");
 	}
 	else {
 		strcpy(newNode->name, string);//copy string array to struct member array
-		newNode->next = (*headPtr);//make new node point to head
-		//make this node the new head
-		(*headPtr) = newNode;
+		newNode->next = (*headPtr);//Make newNode point to what head is currently pointing to
+		(*headPtr) = newNode;//Make the head pointer point to the newNode
 	}
 
 }
@@ -94,19 +93,19 @@ void addNodeAtEnd(struct List** headPtr, char string[]) {
 		printf("Memory not allocated\n");
 	}
 	else {
-		struct List* lastNode = *headPtr;//make lastNode point to the head pointer
+		struct List* lastNode = *headPtr;//Make a pointer to what headPtr is pointing to
 		strcpy(newNode->name, string);//copy string array to struct member array
 		newNode->next = NULL;
 		
-		if (*headPtr == NULL) {//check if there are values in the head
-			*headPtr = newNode;//if there isn't any make new node the head
+		if (*headPtr == NULL) {//Check if there are nodes in the list
+			*headPtr = newNode;//if there isn't make head point to new node
 		}
 		else {
 			//loop through until the next node found is null then we have the pointer of the last node
 			while (lastNode->next != NULL) {
 				lastNode = lastNode->next;
 			}
-			lastNode->next = newNode;//make newNode the last node in the list
+			lastNode->next = newNode;//Make lastNode in list point to NewNode
 		}
 	}
 }
@@ -115,31 +114,32 @@ void deleteNode(struct List** headPtr, char string[]) {
 	struct List* currentNodePtr = *headPtr, *previousNodePtr = NULL;
 
 	if (currentNodePtr != NULL && strcmp(currentNodePtr->name,string) == 0) {//strcmp compares each string character by character
-		*headPtr = currentNodePtr->next;//assign next node in the list the head
-		free(currentNodePtr);//free up current node
+		*headPtr = currentNodePtr->next;//Make headPtr point to what the currentNodePtr points to so we can remove the currentNodePtr
+		free(currentNodePtr);//free up currentNodePtr
 		return;
 	}
 	else {
 		while (currentNodePtr != NULL && strcmp(currentNodePtr->name, string) != 0) {
-			previousNodePtr = currentNodePtr;//hold current node for deletion if name found
+			previousNodePtr = currentNodePtr;//hold currentNodePtr so next node can be deleted if name found in node
 			currentNodePtr = currentNodePtr->next;//get next node in list to check name value
 		}
 		if (currentNodePtr == NULL) {
 			printf("\nNo value found in linked list\n");
 			return;
 		}
-		
-		previousNodePtr->next = currentNodePtr->next;//overwrite previous node address space to point to current node address space
+		//Make previousNodePtr point to what the currentNodePtr points to so we can remove the currentNodePtr
+		previousNodePtr->next = currentNodePtr->next;
 		//free up pointers
 		free(currentNodePtr);
 	}
 }
 
 void printList(struct List *nextNode) {
-	
+	int i = 0;
 	while (nextNode != NULL) {//break when next node is NULL
 
-		printf("Data in node %s\n", nextNode->name);//display value in node
+		printf("Name in node %d is %s\n",i, nextNode->name);//display name value in node
+		i++;
 		nextNode = nextNode->next;//get memory address of next node
 	}
 }
